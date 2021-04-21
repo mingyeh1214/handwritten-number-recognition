2e16b96b712ec115d3867091f90cba47fce146a6
mingyeh1214 <mingyeh1214@gmail.com>
2021-04-21 17:05:53 +0800

    upload

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask
+import tensorflow as tf
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    return 'Hello World!update by github'
 
 if __name__ == "__main__":
     app.run(debug=True)
